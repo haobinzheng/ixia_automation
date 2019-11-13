6074b31ba839279c579bb8cefeece80ce86c26ce
Mike Z <hbz.zheng@gmail.com>
2019-11-13 15:56:06 -0800

    ixia restpy lib basic methods

--- ixia_restpy_lib.py
+++ ixia_restpy_lib.py
@@ -69,7 +69,7 @@ def ixia_rest_connect_chassis(apiServerIp,ixChassisIpList,portList):
         dprint(testPorts)
 
         ixNetwork.AssignPorts(testPorts, [], vportList, forceTakePortOwnership)
-        return ixNetwork,vport_holder_list
+        return testPlatform,session,ixNetwork,vport_holder_list
 
     except Exception as errMsg:
         print('\n%s' % traceback.format_exc(None, errMsg))
@@ -80,24 +80,26 @@ def ixia_rest_connect_chassis(apiServerIp,ixChassisIpList,portList):
 def ixia_rest_create_topology(*args,**kwargs):
     debugMode = False
     try:
-        ixNetwork = kwargs['session']
-        vport1 = kwargs['vport']
+        session = kwargs['session']
+        testPlatform = kwargs['platform']
+        ixNetwork = kwargs['ixnet']
+        vport = kwargs['vport']
         topo_name = kwargs['topo_name']
         dg_name = kwargs['dg_name']
         multi = kwargs['multiplier']
         mac_start = kwargs['mac_start']
         vlan_id = kwargs['vlan_id']       
         ixNetwork.info(f'Creating Topology Group {topo_name}')
-        topology1 = ixNetwork.Topology.add(Name=topo_name, Ports=vport1)
-        deviceGroup1 = topology1.DeviceGroup.add(Name=dg_name, Multiplier=multi)
-        ethernet1 = deviceGroup1.Ethernet.add(Name='Eth1')
-        ethernet1.Mac.Increment(start_value=mac_start, step_value='00:00:00:00:00:01')
+        topology = ixNetwork.Topology.add(Name=topo_name, Ports=vport)
+        deviceGroup = topology.DeviceGroup.add(Name=dg_name, Multiplier=multi)
+        ethernet = deviceGroup.Ethernet.add(Name='Eth1')
+        ethernet.Mac.Increment(start_value=mac_start, step_value='00:00:00:00:00:01')
         #vlan can not be used in FSW, dont know why. need to investigate
         # ethernet1.EnableVlans.Single(True)
 
         # ixNetwork.info('Configuring vlanID')
         # vlanObj = ethernet1.Vlan.find()[0].VlanId.Increment(start_value=vlan_id, step_value=0)
-        return ethernet1
+        return ethernet,topology
     except Exception as errMsg:
         print('\n%s' % traceback.format_exc(None, errMsg))
         if debugMode == False and 'session' in locals():
@@ -107,17 +109,23 @@ def ixia_rest_create_topology(*args,**kwargs):
 def ixia_rest_create_ip(*args,**kwargs):
     debugMode = False
     try:
-        ixNetwork = kwargs['session']
+        session = kwargs['session']
+        testPlatform = kwargs['platform']
+        ixNetwork = kwargs['ixnet']
         start_ip = kwargs['start_ip']
         gw_start_ip = kwargs['gw_start_ip']
         ethernet = kwargs['ethernet']
+        ip_prefix = kwargs['maskbits']
 
         ixNetwork.info('Configuring IPv4')
-        ipv4 = ethernet.Ipv4.add(Name='Ipv4')
+        ipv4 = ethernet.Ipv4.add(Name='IPV4')
         ipv4.Address.Increment(start_value=start_ip, step_value='0.0.0.1')
-        #ipv4.address.RandomMask(mask_value=16)
+        # ipv4.address.RandomMask(fixed_value=16)
         print(dir(ipv4.Address))
         ipv4.GatewayIp.Increment(start_value=gw_start_ip, step_value='0.0.0.1')
+        ipv4.Prefix.Single(ip_prefix)
+        address = ipv4.Address
+        # testPlatform.info(address.prefix)
         return ipv4
     except Exception as errMsg:
         print('\n%s' % traceback.format_exc(None, errMsg))
@@ -132,61 +140,139 @@ def ixia_rest_create_ip(*args,**kwargs):
     #     bgp2 = ipv4.IgmpQuerier.add(Name='igmpQuerier')
 
 def ixia_rest_start_protocols(*args,**kwargs):
-    ixNetwork = kwargs['session']
-    ixNetwork.StartAllProtocols(Arg1='sync')
+    debugMode = False
+    try:
+        session = kwargs['session']
+        testPlatform = kwargs['platform']
+        ixNetwork = kwargs['ixnet']
+        ixNetwork.StartAllProtocols(Arg1='sync')
 
-    ixNetwork.info('Verify protocol sessions\n')
-    protocolsSummary = StatViewAssistant(ixNetwork, 'Protocols Summary')
-    protocolsSummary.CheckCondition('Sessions Not Started', StatViewAssistant.EQUAL, 0)
-    protocolsSummary.CheckCondition('Sessions Down', StatViewAssistant.EQUAL, 0)
-    ixNetwork.info(protocolsSummary)
+        ixNetwork.info('Verify protocol sessions\n')
+        protocolsSummary = StatViewAssistant(ixNetwork, 'Protocols Summary')
+        protocolsSummary.CheckCondition('Sessions Not Started', StatViewAssistant.EQUAL, 0)
+        protocolsSummary.CheckCondition('Sessions Down', StatViewAssistant.EQUAL, 0)
+        ixNetwork.info(protocolsSummary)
+    except Exception as errMsg:
+        print('\n%s' % traceback.format_exc(None, errMsg))
+        if debugMode == False and 'session' in locals():
+            session.remove()
+        return False
 
-    #     ixNetwork.info('Create Traffic Item')
-    #     trafficItem = ixNetwork.Traffic.TrafficItem.add(Name='BGP Traffic', BiDirectional=False, TrafficType='ipv4')
+def ixia_rest_create_traffic(*args,**kwargs):
+    debugMode = False
+    try:
+        session = kwargs['session']
+        testPlatform = kwargs['platform']
+        ixNetwork = kwargs['ixnet']
+        src_topo = kwargs['src']
+        dst_topo = kwargs['dst']
+        traffic_name = kwargs['name']
+        tracking_group = kwargs['tracking_group']
 
-    #     ixNetwork.info('Add endpoint flow group')
-    #     trafficItem.EndpointSet.add(Sources=topology1, Destinations=topology2)
+        ixNetwork.info('Create Traffic Item')
+        trafficItem = ixNetwork.Traffic.TrafficItem.add(Name=traffic_name, BiDirectional=False, TrafficType='ipv4',TransmitMode='sequential')
 
-    #     # Note: A Traffic Item could have multiple EndpointSets (Flow groups).
-    #     #       Therefore, ConfigElement is a list.
-    #     ixNetwork.info('Configuring config elements')
-    #     configElement = trafficItem.ConfigElement.find()[0]
-    #     configElement.FrameRate.update(Type='percentLineRate', Rate=50)
-    #     configElement.TransmissionControl.update(Type='fixedFrameCount', FrameCount=10000)
-    #     configElement.FrameRateDistribution.PortDistribution = 'splitRateEvenly'
-    #     configElement.FrameSize.FixedSize = 128
-    #     trafficItem.Tracking.find()[0].TrackBy = ['flowGroup0']
+        ixNetwork.info('Add endpoint flow group')
+        trafficItem.EndpointSet.add(Sources=src_topo, Destinations=dst_topo)
+        # trafficItem.Tracking.find()[0].TrackBy = [tracking_group]
+        
 
-    #     trafficItem.Generate()
-    #     ixNetwork.Traffic.Apply()
-    #     ixNetwork.Traffic.Start()
+        # # Note: A Traffic Item could have multiple EndpointSets (Flow groups).
+        # #       Therefore, ConfigElement is a list.
+        ixNetwork.info('Configuring config elements')
+        configElement = trafficItem.ConfigElement.find()[0]
+        configElement.FrameRate.update(Type='percentLineRate', Rate=50)
+        #configElement.TransmissionControl.update(Type='fixedFrameCount', FrameCount=10000)
+        configElement.TransmissionControl.update(Type='continuous')
+        configElement.FrameRateDistribution.PortDistribution = 'splitRateEvenly'
+        configElement.FrameSize.FixedSize = 1000
+        
+        trafficItem.Generate()
+        
+        # ixNetwork.Traffic.Apply()
+        # ixNetwork.Traffic.Start()
 
-    #     # StatViewAssistant could also filter by REGEX, LESS_THAN, GREATER_THAN, EQUAL. 
-    #     # Examples:
-    #     #    flowStatistics.AddRowFilter('Port Name', StatViewAssistant.REGEX, '^Port 1$')
-    #     #    flowStatistics.AddRowFilter('Tx Frames', StatViewAssistant.LESS_THAN, 50000)
+        # StatViewAssistant could also filter by REGEX, LESS_THAN, GREATER_THAN, EQUAL. 
+        # Examples:
+        #    flowStatistics.AddRowFilter('Port Name', StatViewAssistant.REGEX, '^Port 1$')
+        #    flowStatistics.AddRowFilter('Tx Frames', StatViewAssistant.LESS_THAN, 50000)
 
-    #     flowStatistics = StatViewAssistant(ixNetwork, 'Flow Statistics')
-    #     ixNetwork.info('{}\n'.format(flowStatistics))
+        # flowStatistics = StatViewAssistant(ixNetwork, 'Flow Statistics')
+        # ixNetwork.info('{}\n'.format(flowStatistics))
 
-    #     for rowNumber,flowStat in enumerate(flowStatistics.Rows):
-    #         ixNetwork.info('\n\nSTATS: {}\n\n'.format(flowStat))
-    #         ixNetwork.info('\nRow:{}  TxPort:{}  RxPort:{}  TxFrames:{}  RxFrames:{}\n'.format(
-    #             rowNumber, flowStat['Tx Port'], flowStat['Rx Port'],
-    #             flowStat['Tx Frames'], flowStat['Rx Frames']))
+        # for rowNumber,flowStat in enumerate(flowStatistics.Rows):
+        #     ixNetwork.info('\n\nSTATS: {}\n\n'.format(flowStat))
+        #     ixNetwork.info('\nRow:{}  TxPort:{}  RxPort:{}  TxFrames:{}  RxFrames:{}\n'.format(
+        #         rowNumber, flowStat['Tx Port'], flowStat['Rx Port'],
+        #         flowStat['Tx Frames'], flowStat['Rx Frames']))
 
-    #     flowStatistics = StatViewAssistant(ixNetwork, 'Traffic Item Statistics')
-    #     ixNetwork.info('{}\n'.format(flowStatistics))
+        # flowStatistics = StatViewAssistant(ixNetwork, 'Traffic Item Statistics')
+        # ixNetwork.info('{}\n'.format(flowStatistics))
 
-    #     if debugMode == False:
-    #         # For linux and connection_manager only
-    #         session.remove()
+    except Exception as errMsg:
+        print('\n%s' % traceback.format_exc(None, errMsg))
+        if debugMode == False and 'session' in locals():
+            session.remove()
 
-    # except Exception as errMsg:
-    #     print('\n%s' % traceback.format_exc(None, errMsg))
-    #     if debugMode == False and 'session' in locals():
-    #         session.remove()
+def ixia_rest_start_traffic(*args,**kwargs):
+    session = kwargs['session'] 
+    testPlatform = kwargs['platform']
+    ixNetwork = kwargs['ixnet']
+    trafficItem = ixNetwork.Traffic.TrafficItem
+    for item in trafficItem.find():
+        testPlatform.info(item)
+        tracking = item.Tracking.find()
+        testPlatform.info(tracking)
+        tracking.TrackBy = [f"flowGroup0",f"trackingenabled0"]
+    ixNetwork.Traffic.Apply()
+    ixNetwork.Traffic.Start()
 
+ 
+def ixia_rest_collect_stats(*args,**kwargs):
+    debugMode = False
+    try:
+        session = kwargs['session']
+        testPlatform = kwargs['platform']
+        ixNetwork = kwargs['ixnet']
+
+        #The following line print out all rows stats at once
+        flowStatistics = StatViewAssistant(ixNetwork, 'Flow Statistics')
+        print(flowStatistics)
+        print(dir(flowStatistics))
+        print(type(flowStatistics))
+        print("--------------------------------------------------------")
+        print(flowStatistics.Rows)
+        print(dir(flowStatistics.Rows))
+        print(type(flowStatistics))
+        # ixNetwork.info('{}\n'.format(flowStatistics))
+        flow_stats_list = []
+        for rowNumber,flowStat in enumerate(flowStatistics.Rows):
+            flowStat_dict = {}
+            ixNetwork.info('\n\nSTATS: {}\n\n'.format(flowStat))
+            ixNetwork.info('\nRow:{}  TxPort:{}  RxPort:{}  TxFrames:{}  RxFrames:{}  Delta:{} Tx Rate:{}\n'.format(
+                rowNumber, flowStat['Tx Port'], flowStat['Rx Port'],
+                flowStat['Tx Frames'], flowStat['Rx Frames'],flowStat["Frames Delta"],flowStat["Tx Rate (Bps)"]))
+            flowStat_dict['Tx Port'] = flowStat['Tx Port'] 
+            flowStat_dict['Tx Port'] = flowStat['Tx Port']
+            flowStat_dict['Tx Frames'] = flowStat['Tx Frames']
+            flowStat_dict['Rx Frames'] = flowStat['Rx Frames']
+            flowStat_dict["Frames Delta"] = flowStat["Frames Delta"]
+            flowStat_dict["Tx Rate (Bps)"] = flowStat["Tx Rate (Bps)"]
+            flowStat_dict['Traffic Item'] =  flowStat['Traffic Item']
+            flowStat_dict['Flow Group'] =    flowStat['Flow Group']
+            flowStat_dict['Tx Frame Rate'] = flowStat['Tx Frame Rate']
+            flowStat_dict['Tx Rate (bps)'] = flowStat['Tx Rate (bps)']
+            flow_stats_list.append(flowStat_dict)
+
+        print(flow_stats_list)
+        return flow_stats_list
+
+        # flowStatistics = StatViewAssistant(ixNetwork, 'Traffic Item Statistics')
+        # ixNetwork.info('{}\n'.format(flowStatistics))
+    except Exception as errMsg:
+        print('\n%s' % traceback.format_exc(None, errMsg))
+        if debugMode == False and 'session' in locals():
+            session.remove()
 
 
 if __name__ == "__main__":
@@ -194,49 +280,89 @@ if __name__ == "__main__":
     ixChassisIpList = ['10.105.241.234']
     
     portList = [[ixChassisIpList[0], 4,15], [ixChassisIpList[0], 4, 16]]
-    ixNetwork,vport_holder_list = ixia_rest_connect_chassis(apiServerIp,ixChassisIpList,portList)
+    testPlatform,Session,ixNetwork,vport_holder_list = ixia_rest_connect_chassis(apiServerIp,ixChassisIpList,portList)
     if ixNetwork == False:
         print(f"Error connected to IXIA chassis {ixChassisIpList}")
         exit()
-    ethernet_session = ixia_rest_create_topology(
-        session = ixNetwork,
+    ethernet1,topology1 = ixia_rest_create_topology(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
         vport = vport_holder_list[0],
         topo_name = "Topology_1",   
         dg_name = "DG1",    
-        multiplier = 1000,    
+        multiplier = 100,    
         mac_start = "00:11:01:01:01:01",
         vlan_id = 1
     )     
 
-    if ethernet_session == False:
+    if ethernet1 == False:
         print(f"Error creating topology on chassis {ixChassisIpList} port {vport_holder_list[0]}")
         exit()
     ip_session = ixia_rest_create_ip(
-        session = ixNetwork,
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
         start_ip = "10.1.1.1",
         gw_start_ip = "10.2.1.1",
-        ethernet = ethernet_session,
+        ethernet = ethernet1,
+        maskbits = 16,
     )
-
-    ethernet_session = ixia_rest_create_topology(
-        session = ixNetwork,
+    ethernet2,topology2 = ixia_rest_create_topology(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
         vport = vport_holder_list[1],
         topo_name = "Topology_2",   
         dg_name = "DG2",    
-        multiplier = 1000,    
+        multiplier = 100,    
         mac_start = "00:12:01:01:01:01",
         vlan_id = 1
     )     
 
-    if ethernet_session == False:
+    if ethernet2 == False:
         print(f"Error creating topology on chassis {ixChassisIpList} port {vport_holder_list[0]}")
         exit()
     ip_session = ixia_rest_create_ip(
-        session = ixNetwork,
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
         start_ip = "10.2.1.1",
         gw_start_ip = "10.1.1.1",
-        ethernet = ethernet_session,
+        ethernet = ethernet2,
+        maskbits = 16,
     )
 
-    ixia_rest_start_protocols(session = ixNetwork)
-    
+    ixia_rest_start_protocols(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
+    )
+    ixia_rest_create_traffic(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
+        src = topology1,
+        dst = topology2,
+        name= 'topo1_to_topo2',
+        tracking_group = 'flowGroup0',
+    )
+    ixia_rest_create_traffic(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
+        src = topology2,
+        dst = topology1,
+        name= 'topo2_to_topo1',
+        tracking_group = 'flowGroup1',
+    )
+    ixia_rest_start_traffic(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork,
+        )
+    ixia_rest_collect_stats(
+        platform = testPlatform, 
+        session = Session,
+        ixnet = ixNetwork, 
+    )
