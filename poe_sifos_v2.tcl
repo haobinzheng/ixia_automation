cc3d04c8f1ef0331054fe7d5fad4a3500f22b1a9
Haobin Zheng <zhengh@fortinet-us.com>
2023-01-20 13:04:04 -0800

    sync

--- poe_sifos_v2.tcl
+++ poe_sifos_v2.tcl
@@ -1,4 +1,5 @@
 psa 10.105.241.47
+psa 10.105.241.49
 psa_4pair 5,1 single
 
 Model                           SN                        APC                                                  Rack            Mgmt IP
@@ -14,12 +15,39 @@ IP                         Crednetials
 10.105.252.23   Adminstrator/Fortinet123!
 10.105.252.25   Adminstrator/Fortinet123!
 
-
-
-
+proc capture_peak {} {
+# Configure test port for valid signature 
+passive 1,1 r 26 c 0
+# Configure Vpeak Measurement
+vdcpeak 1,1 trig on max period 200m timeout 10
+# Sync to trailing edge of pre-detection pulse (uses Trigger 1 !)
+psa_det_sync 1,1 falling level 2.5
+# Setup trigger for Vvalid(Max) measurement
+trig1 1,1 rising
+# Connect the detection resistance by connecting the port switch
+port 1,1 connect
+# Recover the peak voltage of the detection pulse
+set status [psa_wait 1,1 vdcpeak]
+set VvalidMax [lindex $status 3]
+# Force a power-down by opening port switch, setting load to zero
+psa_disconnect 1,1 
+# Configure the Time Interval Measurement
+# Trigger level and polarity of falling at level 2 also used for sync
+timint 1,1 start falling level 2 stop rising level 40 msec noisy
+# Synchronize to the end of pre-detection pulse
+psa_det_sync 1,1 falling level 2 noisy
+# Connect the detection resistance by connecting the port switch
+port 1,1 connect
+# Recover the time from end of detection until power-up
+set status [psa_wait 1,1 timint]
+set Tpon [lindex $status 4]
+puts "Vvalid_Max = $VvalidMax volts, Tpon = $Tpon msec"
+}
 
 proc psl_power_up {} {
-	set port_list {"1,2","2,2","3,2","4,2","5,2","6,2","7,2","8,2" "9,2","10,2","11,2","12,2","13,2","14,2","15,2","16,2"}
+	#set port_list {"1,2","2,2","3,2","4,2","5,2","6,2","7,2","8,2" "9,2","10,2","11,2","12,2","13,2","14,2","15,2","16,2"}
+	#set port_list {"1,1","2,1","3,1","4,1","5,1","6,1","7,1","8,1" "9,1","10,1","11,1","12,1","13,1","14,1","15,1","16,1","17,1","18,1","19,1","20,1","21,1","22,1","23,1","24,1"}
+    set port_list {"1,2","2,2","3,2","4,2","5,2","6,2","7,2","8,2" "9,2","10,2","11,2","12,2","13,2","14,2","15,2","16,2","17,2","18,2","19,2","20,2","21,2","22,2","23,2","24,2"}
 	set power_list {13.7 3.9 6.7 13.7 28.7} 
     set class_list {0 1 2 3 4}
     set zipped [lmap a $class_list b $power_list {list $a $b}]
@@ -28,7 +56,7 @@ proc psl_power_up {} {
 	regsub -all {(,")|(",)|"} $port_list " " port_list;
 	puts $port_list
 	puts $power_list
-	for {set i 0} {$i < 1} {incr i} {
+	for {set i 0} {$i < 1000} {incr i} {
 		foreach z $zipped {
             set class [lindex $z 0]
             puts $class
@@ -37,6 +65,8 @@ proc psl_power_up {} {
 			foreach port $port_list {
 				# puts $port 
 				# puts $power
+				puts "psl_setup $port 2pA"
+				psl_setup $port 2pA
 				puts "power_pse $port c $class p $power"
         		set status [power_pse $port c $class p $power]
                 puts $status
@@ -102,7 +132,7 @@ proc bt_power_basic {} {
  	regsub -all {(,")|(",)|"} $port_list " " port_list;
 	puts $port_list
 	puts $power_list
-	for {set i 0} {$i < 1} {incr i} {
+	for {set i 0} {$i < 1000} {incr i} {
 			foreach z $zipped {
                 set class [lindex $z 0]
                 puts $class
