0703595856ce1696277b6e2ed41d1b31226d7ddc
Mike Zheng <haobin.zheng08@gmail.com>
2023-03-01 13:00:42 -0800

    import wexpect

--- poe_sifo.py
+++ poe_sifo.py
@@ -3,7 +3,13 @@ import wexpect
 # Spawn a new process for the TCL shell
 #tcl_shell = wexpect.spawn('/c/Program\\ Files\\ (x86)/Sifos/PSA3000/PowerShell\\ TCL.exe')
 tcl_shell = wexpect.spawn('winpty ./powershell_tcl.exe')
-tcl_shell.sendline('\n\n\n\n')
+tcl_shell.sendline('\n')
+sleep(1)
+tcl_shell.sendline('\n')
+sleep(1)
+tcl_shell.sendline('\n')
+sleep(1)
+tcl_shell.sendline('\n')
 tcl_shell.sendline('puts "Hello, world!"')
 # Wait for the TCL prompt to appear
 tcl_shell.expect('>')
@@ -12,7 +18,7 @@ tcl_shell.expect('>')
 tcl_shell.sendline('puts "Hello, world!"')
 output = tcl_shell.before
 print(output)
-tcl_shell.sendline('psa_test_load 3,2 fast c 4 -force t 20')
+tcl_shell.sendline('psa_test_load 1,1 fast c 4 -force t 20')
 tcl_shell.expect('>')
 print(tcl_shell.before)
 exit()
