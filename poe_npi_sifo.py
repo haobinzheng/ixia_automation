0535f3799dabd8bddda76420f442f488dd0c1552
Mike Z <haobin.zheng08@gmail.com>
2023-03-04 13:49:34 -0800

    first try to change function

--- poe_npi_sifo.py
+++ poe_npi_sifo.py
@@ -9,6 +9,76 @@ import jinja2.ext
 def jinja2_zip(*args):
     return zip(*args)
 
+def scan_get_poe_inline(test):
+	for tcl in test.tcl_procedure_list:
+		if type(tcl) == dict:
+			tcl = dict2obj(tcl)
+		print(tcl)
+		print(tcl.proc_name)
+		print(tcl.commands)
+		print(tcl.poe_class)
+		if tcl.execute == False:
+			continue
+		if new_power_shell:
+			tprint(f"Launching Power Shell TCL with PSA IP = {tcl.sifo_ip}")
+			pshell = power_shell_tcl(tcl.sifo_ip)
+			new_power_shell = False
+		pshell.tcl_send_commands_direct(tcl.disconnect_commands,"disconnect")
+		sleep(60)
+		pshell.tcl_send_commands_direct(tcl.commands,tcl.proc_name)
+		poe_inline_dict = {}
+		start_time = time.time()
+		while True:
+			if time.time() - start_time >  timer:
+				ErrorNotify(f"Failed After {timer} seconds:  test case {test.case_name} |  TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}: Not to deliever power to all switch ports {test.dut_port_list}")
+				break
+			sleep(10)
+			#sw.print_show_command("get switch poe inline")
+			output = collect_show_cmd(sw.console,"get switch poe inline")
+			for line in output:
+				for p in test.dut_port_list:
+					if p in line and "Delivering Power" in line and str(tcl.poe_class) in line :
+						items = line.split()
+						dprint(items)
+						portname = items[0]
+						status = items[1]
+						state = f"{items[2]} {items[3]}"
+						max_power = items[4]
+						power_comsumption = items[5]
+						priority = items[6]
+						poe_class = items[7]
+						poe_inline_dict.setdefault(portname,{}) 
+						poe_inline_dict[portname]["status"] =status
+						poe_inline_dict[portname]["state"] = state
+						poe_inline_dict[portname]["max_power"] = max_power
+						poe_inline_dict[portname]["power_comsumption"] = power_comsumption
+						poe_inline_dict[portname]["priority"] = priority
+						poe_inline_dict[portname]["poe_class"] = poe_class
+						dprint(float(poe_inline_dict[portname]["max_power"]),float(tcl.max_power))
+						if float(poe_inline_dict[portname]["max_power"]) != float(tcl.max_power):
+							poe_inline_dict[portname]["powered"] = False
+						else:
+							poe_inline_dict[portname]["powered"] = True
+									 
+			print_dict(poe_inline_dict)
+			ports = poe_inline_dict.keys()
+			result = True
+			if set(ports) != set(test.dut_port_list):
+				result = False
+				continue
+			for p,p_status in poe_inline_dict.items():
+				if p_status["powered"] == False:
+					print(f'{p} poe checking result = {p_status["powered"]}')
+					result = False
+					break
+			if result == True:
+				tprint(f"PASSED: test case {test.case_name} | TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}")
+				break
+		if tcl.tcl_close == True:
+			pshell.tcl_close_shell()
+			new_power_shell = True
+
+
 if __name__ == "__main__":
 	sys.stdout = Logger("Log/poe_bt_testing.log")
 	
@@ -40,7 +110,7 @@ if __name__ == "__main__":
 	 
 	# Write the dictionary to a YAML file
 	with open('yaml_testcase/poe_npi_testing.yaml', 'w') as f:
-			yaml.dump(yaml_dict, f)
+		yaml.dump(yaml_dict, f)
 	#Use this newly generated yaml file to set up testing
 	setup = test_setup("yaml_testcase/poe_npi_testing.yaml")
 	#setup.pretty_print()
@@ -50,7 +120,6 @@ if __name__ == "__main__":
 	testtopo_file = 'topo_poe_npi_6xx.xml'
 	parse_testtopo_untangle(testtopo_file,tb)
 	tb.show_tbinfo()
-
 
 	switches = []
 	devices=[]
@@ -75,77 +144,78 @@ if __name__ == "__main__":
 		print(test.class_list)
 		print(test.poe_port_list)
 		
-		# Start the timer
-		for tcl in test.tcl_procedure_list:
-			if type(tcl) == dict:
-				tcl = dict2obj(tcl)
-			print(tcl)
-			print(tcl.proc_name)
-			print(tcl.commands)
-			print(tcl.poe_class)
-			if tcl.execute == False:
-				continue
-			if new_power_shell:
-				tprint(f"Launching Power Shell TCL with PSA IP = {tcl.sifo_ip}")
-				pshell = power_shell_tcl(tcl.sifo_ip)
-				new_power_shell = False
-			pshell.tcl_send_commands_direct(tcl.disconnect_commands,"disconnect")
-			sleep(60)
-			pshell.tcl_send_commands_direct(tcl.commands,tcl.proc_name)
-			poe_inline_dict = {}
-			start_time = time.time()
-			while True:
-				if time.time() - start_time >  timer:
-					ErrorNotify(f"Failed After {timer} seconds:  test case {test.case_name} |  TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}: Not to deliever power to all switch ports {test.dut_port_list}")
-					break
-				sleep(10)
-				#sw.print_show_command("get switch poe inline")
-				output = collect_show_cmd(sw.console,"get switch poe inline")
-				for line in output:
-					for p in test.dut_port_list:
-						if p in line and "Delivering Power" in line and str(tcl.poe_class) in line :
-							items = line.split()
-							dprint(items)
-							portname = items[0]
-							status = items[1]
-							state = f"{items[2]} {items[3]}"
-							max_power = items[4]
-							power_comsumption = items[5]
-							priority = items[6]
-							poe_class = items[7]
-							poe_inline_dict.setdefault(portname,{}) 
-							poe_inline_dict[portname]["status"] =status
-							poe_inline_dict[portname]["state"] = state
-							poe_inline_dict[portname]["max_power"] = max_power
-							poe_inline_dict[portname]["power_comsumption"] = power_comsumption
-							poe_inline_dict[portname]["priority"] = priority
-							poe_inline_dict[portname]["poe_class"] = poe_class
-							dprint(float(poe_inline_dict[portname]["max_power"]),float(tcl.max_power))
-							if float(poe_inline_dict[portname]["max_power"]) != float(tcl.max_power):
-								poe_inline_dict[portname]["powered"] = False
-							else:
-								poe_inline_dict[portname]["powered"] = True
+		python_verify_func = test['python_verify_func']
+		result = python_verify_func(test)
+ 		# for tcl in test.tcl_procedure_list:
+		# 	if type(tcl) == dict:
+		# 		tcl = dict2obj(tcl)
+		# 	print(tcl)
+		# 	print(tcl.proc_name)
+		# 	print(tcl.commands)
+		# 	print(tcl.poe_class)
+		# 	if tcl.execute == False:
+		# 		continue
+		# 	if new_power_shell:
+		# 		tprint(f"Launching Power Shell TCL with PSA IP = {tcl.sifo_ip}")
+		# 		pshell = power_shell_tcl(tcl.sifo_ip)
+		# 		new_power_shell = False
+		# 	pshell.tcl_send_commands_direct(tcl.disconnect_commands,"disconnect")
+		# 	sleep(60)
+		# 	pshell.tcl_send_commands_direct(tcl.commands,tcl.proc_name)
+		# 	poe_inline_dict = {}
+		# 	start_time = time.time()
+		# 	while True:
+		# 		if time.time() - start_time >  timer:
+		# 			ErrorNotify(f"Failed After {timer} seconds:  test case {test.case_name} |  TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}: Not to deliever power to all switch ports {test.dut_port_list}")
+		# 			break
+		# 		sleep(10)
+		# 		#sw.print_show_command("get switch poe inline")
+		# 		output = collect_show_cmd(sw.console,"get switch poe inline")
+		# 		for line in output:
+		# 			for p in test.dut_port_list:
+		# 				if p in line and "Delivering Power" in line and str(tcl.poe_class) in line :
+		# 					items = line.split()
+		# 					dprint(items)
+		# 					portname = items[0]
+		# 					status = items[1]
+		# 					state = f"{items[2]} {items[3]}"
+		# 					max_power = items[4]
+		# 					power_comsumption = items[5]
+		# 					priority = items[6]
+		# 					poe_class = items[7]
+		# 					poe_inline_dict.setdefault(portname,{}) 
+		# 					poe_inline_dict[portname]["status"] =status
+		# 					poe_inline_dict[portname]["state"] = state
+		# 					poe_inline_dict[portname]["max_power"] = max_power
+		# 					poe_inline_dict[portname]["power_comsumption"] = power_comsumption
+		# 					poe_inline_dict[portname]["priority"] = priority
+		# 					poe_inline_dict[portname]["poe_class"] = poe_class
+		# 					dprint(float(poe_inline_dict[portname]["max_power"]),float(tcl.max_power))
+		# 					if float(poe_inline_dict[portname]["max_power"]) != float(tcl.max_power):
+		# 						poe_inline_dict[portname]["powered"] = False
+		# 					else:
+		# 						poe_inline_dict[portname]["powered"] = True
  									 
-				print_dict(poe_inline_dict)
-				ports = poe_inline_dict.keys()
-				result = True
-				if set(ports) != set(test.dut_port_list):
-					result = False
-					continue
-				for p,p_status in poe_inline_dict.items():
-					if p_status["powered"] == False:
-						print(f'{p} poe checking result = {p_status["powered"]}')
-						result = False
-						break
-				if result == True:
-					tprint(f"PASSED: test case {test.case_name} | TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}")
-					break
+		# 		print_dict(poe_inline_dict)
+		# 		ports = poe_inline_dict.keys()
+		# 		result = True
+		# 		if set(ports) != set(test.dut_port_list):
+		# 			result = False
+		# 			continue
+		# 		for p,p_status in poe_inline_dict.items():
+		# 			if p_status["powered"] == False:
+		# 				print(f'{p} poe checking result = {p_status["powered"]}')
+		# 				result = False
+		# 				break
+		# 		if result == True:
+		# 			tprint(f"PASSED: test case {test.case_name} | TCL procedure {tcl.proc_name} | POE Class {tcl.poe_class}")
+		# 			break
 					 
 				# keyin = input("Please check poe inline output. Do you want to finish showing this command?(Y/N): ")
 				# if keyin.upper() == "Y":
 				# 	break
 				
-			if tcl.tcl_close == True:
-				pshell.tcl_close_shell()
-				new_power_shell = True
+			# if tcl.tcl_close == True:
+			# 	pshell.tcl_close_shell()
+			# 	new_power_shell = True
 	pshell.tcl_close_shell()
